db09540ad94cea0866ce6fa614faff49b6a65033
JOMAR NACORDA <jnacorda04@gmail.com>
2025-01-05 23:34:58 +1300

    Day 1 of transferring to react

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <head>
+            <title>Jomar Project</title>
+            <meta charset="utf-8" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, user-scalable=no"
+            />
+            <link rel="stylesheet" href="css\main.css" />
+            <link
+                rel=" stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+            />
+            <link rel="icon" href="images/favicon.ico" type="images/x-icon" />
+            <noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
+        </head>
+    )
+}
