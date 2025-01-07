92f1adb50ead247524143443abfbe16d5bed010d
JOMAR NACORDA <jnacorda04@gmail.com>
2025-01-07 18:54:29 +1300

    Add initial React components and styles for the application

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
+
+
 function Header() {
     return (
-        <head>
+        <div>
             <title>Jomar Project</title>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1, user-scalable=no"
             />
-            <link rel="stylesheet" href="css\main.css" />
+            <link rel="stylesheet" href="./main.css" />
             <link
                 rel=" stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
             />
             <link rel="icon" href="images/favicon.ico" type="images/x-icon" />
-            <noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
-        </head>
+            <noscript><link rel="stylesheet" href="/noscript.css" /></noscript>
+        </div>
     )
 }
+
+export default Header;
